ed52263377af796aab414d67d7d987d368d06c81
Jackson Gray <nohaxjustdoge@gmail.com>
2024-04-01 05:21:39 +0000

    py

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import Button from "./Button.jsx";
 function App() {
   return (
     <div className="h-screen bg-zinc-800 flex-col justify-center w-full text-center">
-      <Example/>
+      <Button/>
     </div>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,20 @@
-export default function Example() {
+export default function Button() {
+
+  function handleClick() {
+    fetch('http://67.164.191.36:5000/api/hello')
+      .then(r => r.json())
+      .then(response => console.log(response))
+  }
+
   return (
     <>
-    <button
+      <button
         type="button"
         className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        onClick={()=>console.log("button clicked")}
-        >
+        onClick={() => handleClick()}
+      >
         Button text
-    </button>
+      </button>
     </>
-    )
+  )
 }
